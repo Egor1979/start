a3b8ba4cd6b2d9dbdbf5d749fed1432d1f9ac162
Georgy <kegora9@gmail.com>
2021-12-31 11:05:12 +0300

    i made a header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-
+$(function(){//поключил слайдер и до возмю.
+  $( '.header-slider').slick({
+dots: true,prevArrow: '<button type="button" class="slick-prev"><img src="imajes/prev.svg" alt=""></button>',
+nextArrow:'<button type="button" class="slick-next"><img src="imajes/next.svg" alt=""></button>'
+//дотс тру это взято из натсроек слайдера на слик внизу саита это вызвало цифры
+//стрелки в слайдере настройка,я пропис.в своб.месте хтмл адрес вырезал ископир.сюда
+  })
 });
