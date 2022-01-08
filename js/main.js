a069a5bc1ccdc390db5222b06b5e5f904bea87d5
Georgy <kegora9@gmail.com>
2022-01-08 11:48:20 +0300

    current

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-$(function(){//поключил слайдер и до возмю.
-  $( '.header-slider').slick({
-dots: true,prevArrow: '<button type="button" class="slick-prev"><img src="imajes/prev.svg" alt=""></button>',
-nextArrow:'<button type="button" class="slick-next"><img src="imajes/next.svg" alt=""></button>'
-//дотс тру это взято из натсроек слайдера на слик внизу саита это вызвало цифры
-//стрелки в слайдере настройка,я пропис.в своб.месте хтмл адрес вырезал ископир.сюда
-  })
+$(function () {//поключил слайдер и до возмю.
+
+  $('.header-slider').slick({
+    dots: true, prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt="l"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt="l"></button>'
+    //дотс тру это взято из натсроек слайдера на слик внизу саита это вызвало цифры
+    //стрелки в слайдере настройка,я пропис.в своб.месте хтмл адрес вырезал ископир.сюда
+  });
 });
