27ec219996ceea5ae5df1435baf4417e92d90211
Georgy <kegora9@gmail.com>
2022-01-08 21:29:35 +0300

    связл слайдеры

--- js/main.js
+++ js/main.js
@@ -7,3 +7,18 @@ $(function () {//поключил слайдер и до возмю.
     //стрелки в слайдере настройка,я пропис.в своб.месте хтмл адрес вырезал ископир.сюда
   });
 });
+
+$('.product__name').slick({
+  slidesToShow: 6,//всего слаидов
+  slidesToScroll: 1,//пролист за раз
+  focusOnSelect: true//чтобы при нажатии связаная картинка появилась
+   asNavFor: '.product__content'//связал слайдеры
+});
+$('.product__content').slick({
+  /*slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  */
+});
