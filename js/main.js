2c2948c42267204b196f6760a2d6de7cd571bd85
Georgy <kegora9@gmail.com>
2022-01-09 15:13:39 +0300

    НЕ МОГУ ПОНЯТЬ ПОЧЕМУ НЕ РАБОТАЕТ СЛАЙДЕР

--- js/main.js
+++ js/main.js
@@ -2,23 +2,26 @@ $(function () {//поключил слайдер и до возмю.
 
   $('.header-slider').slick({
     dots: true, prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt="l"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt="l"></button>'
+    nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt="l"></button>',
+    fade: true
     //дотс тру это взято из натсроек слайдера на слик внизу саита это вызвало цифры
     //стрелки в слайдере настройка,я пропис.в своб.месте хтмл адрес вырезал ископир.сюда
   });
-});
+
 
 $('.product__name').slick({
   slidesToShow: 6,//всего слаидов
   slidesToScroll: 1,//пролист за раз
-  focusOnSelect: true//чтобы при нажатии связаная картинка появилась
+  focusOnSelect: true,//чтобы при нажатии связаная картинка появилась
    asNavFor: '.product__content'//связал слайдеры
+   vertical:true
 });
 $('.product__content').slick({
-  /*slidesToShow: 3,
+  slidesToShow: 1,
   slidesToScroll: 1,
-  asNavFor: '.slider-for',
-  dots: true,
-  centerMode: true,
-  */
+  asNavFor: '.product__name',
+ fade: true
+  
+});
+
 });
