1660f2ade9eeae1fd251783cb953a5465f734d48
Georgy <kegora9@gmail.com>
2022-03-01 14:56:33 +0300

    НЕ МОГУ РАЗОБРАТЬСЯ С ДЖС .ПРОДУКТ НЕЙМ НА 890 БРЕКПОЙНТ НЕ СТАНОВИТСЯ В СТРОКУ.

--- js/main.js
+++ js/main.js
@@ -5,10 +5,22 @@ $(function () {
     dots: true, prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt=""></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
     fade: true,
-
+    responsive:[
+      {
+      breakpoint:361,
+      settings:{
+        dots:false,
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed: 2000,
+      }
+    }
+      
+    ]
+  });
     //дотс тру это взято из натсроек слайдера на слик внизу саита это вызвало цифры
     //стрелки в слайдере настройка,я пропис.в своб.месте хтмл адрес вырезал ископир.сюда
-  });
+  
 
 
   $('.product__name').slick({
@@ -19,6 +31,25 @@ $(function () {
     vertical: true,
     prevArrow: '<button type="button" class="product-prev"><img src="images/product-prevu.png" alt=""></button>',
     nextArrow: '<button type="button" class="product-next"><img src="images/product-next.png" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 891,/*НА 1 БОЛЬШЕ ПИКСЕЛИ ПИШЕШЬ */
+        settings: {
+          vertical: false,
+          slidesToShow:3,
+          arrows:false,
+          
+        },
+        breakpoint:461,
+        settings: {
+          vertical:   false,  
+          slidesToShow: 1,
+          arrows: false,
+          dots:true
+        
+        } 
+      }
+    ]
   });
   
   $('.product__content').slick({
@@ -28,5 +59,9 @@ $(function () {
     fade: true,
     arrows: false
 
+  });
+  //БУРГЕР КНОПКУ ПРИВЯЗЫВАЮ К МЕНЮ ЛИСТ .ТОГЛКЛАСС ЧТОБ ИСЧ.ПРИ ПОВТ НАЖ МЕН.
+  $('.menu__btn').on('click',function(){
+    $('.menu__list').toggleClass('menu__list--active');
   });
 });
